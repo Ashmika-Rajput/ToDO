0fb9ddd826ba6037e801836f0491de66feda7247
Ashmika <ashmika1194@gmail.com>
2022-05-20 14:50:43 +0530

    test update

--- src/components/AddTask.test.js
+++ src/components/AddTask.test.js
@@ -2,28 +2,27 @@ import { fireEvent, render,screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import AddTask from './AddTask';
 
+/*Testing
+    -whether user is allowed to add todo in input field or not
+    -whether input field changes to empty once todo is added
+    -whether single todo is rendered on screen properly or not
+    -whether multiple todos is rendered on screen properly or not
+*/   
+const addTodo=(todo)=>{
+    const inputElement=screen.getByPlaceholderText('Add todo...');
+    const buttonElement=screen.getByRole('button',{name:'Create Task'});
+    todo.forEach(todo=>{
+        fireEvent.change(inputElement,{target:{value:todo}});
+        fireEvent.click(buttonElement);
+    })
+}
 
 describe('Add task Component', () => {
-    test('render To Do',()=>{
-        //Arrange
-          render(<AddTask/>);
-        //Assert
-          const taskElement = screen.getByText('ToDo');
-          expect(taskElement).toBeInTheDocument();
-    })
-    test('renders "Create task" button is enabled or not ',()=>{
-        //Arrange
-           render(<AddTask/>);
-        //Assert
-           const outputElement=screen.getByRole('button',{name:'Create Task'})
-           expect(outputElement).toBeEnabled();
-
-    })
-    test('renders input field changes',()=>{
+    test('renders input field accepting user input or not',()=>{
        //Arrange   
            render(<AddTask/>);
        //Act
-           const inputElement=screen.getByPlaceholderText('Add todo...','Create 1st task');
+           const inputElement=screen.getByPlaceholderText('Add todo...','Go Shopping');
            userEvent.type(inputElement);
        //Assert
            expect(inputElement).toBeInTheDocument();   
@@ -39,31 +38,20 @@ describe('Add task Component', () => {
         //Assert 
             expect(inputElement.value).toBe("");   
      })
-})
-//INTEGRATION TEST
-const addTask=(task)=>{
-    const inputElement=screen.getByPlaceholderText('Add todo...');
-    const buttonElement=screen.getByRole('button',{name:'Create Task'});
-    task.forEach(task=>{
-        fireEvent.change(inputElement,{target:{value:task}});
-        fireEvent.click(buttonElement);
-    })
-}
-describe('Add Component',()=>{
-    test('render display single task as list upon creating task',()=>{
+     test('render single todo on screen',()=>{
         //Arrange
            render(<AddTask/>);
         //Act
-           addTask(['Go Shopping']);
+           addTodo(['Go Shopping']);
         //Assert
            const divElement=screen.getByText('Go Shopping');
            expect(divElement).toBeInTheDocument();
     })
-    test('render display multiple task as list upon creating task',()=>{
+    test('render multiple todos on screen',()=>{
         //Arrange
            render(<AddTask/>);
         //Act
-           addTask(['Go Shopping','Pay Bill','Rent a Flat']);
+           addTodo(['Go Shopping','Pay Bill','Rent a Flat']);
         //Assert
            const divElement=screen.getAllByTestId("task");
            expect(divElement.length).toBe(5);
